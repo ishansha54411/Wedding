dbe80f9eb1d8fac5d907e49fdc679ebca30a88e7
ishan <ishan98.es@gamil.com>
2023-01-16 10:45:15 +0530

    Countdown updated

--- src/sub-components/CountDown.js
+++ src/sub-components/CountDown.js
@@ -2,10 +2,35 @@ import React from 'react'
 import Countdown from 'react-countdown';
 
 const CountDown=({date})=>{
+    const getRemainingMonths=(date)=>{
+        let months=0;
+        let newDate=new Date();
+        newDate.setMonth(newDate.getMonth()+1);
+        while(newDate<date)
+        {
+            months++;
+            newDate.setMonth(newDate.getMonth()+1)
+        } 
+        // console.log(newDate.getMonth());
+        return months;
+    }
+    const getRemainingDays=()=>{
+        // let newDate=new Date();
+        let newDate=new Date();
+        newDate.setMonth(newDate.getMonth()+1);
+        while(newDate<date)
+        {
+            newDate.setMonth(newDate.getMonth()+1)
+        }
+        newDate.setMonth(newDate.getMonth()-1)
+        return Math.ceil(Math.abs(newDate-date)/ (1000 * 60 * 60 * 24))-1;
+
+    }
     const Completionist = () => <span>Already Married!</span>;
 
     // Renderer callback with condition
     const renderer = ({ days,hours, minutes, seconds, completed }) => {
+        
       if (completed) {
         // Render a completed state
         return <Completionist />;
@@ -15,7 +40,13 @@ const CountDown=({date})=>{
             <div className="simply-countdown simply-countdown-one">
                 <div className="simply-section simply-days-section">
                     <div>
-                        <span className="simply-amount">{days}</span>
+                        <span className="simply-amount">{getRemainingMonths(date)}</span>
+                        <span className="simply-word">months</span>
+                    </div>
+                </div>
+                <div className="simply-section simply-days-section">
+                    <div>
+                        <span className="simply-amount">{getRemainingDays(date)}</span>
                         <span className="simply-word">days</span>
                     </div>
                 </div>
